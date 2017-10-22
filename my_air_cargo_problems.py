b580a7fa531070d4fc26e8d35aebacfe90b0fedc
lyzl <lyzl5764@hotmail.com>
2017-10-22 03:23:08 +0100

    add missed function

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -196,7 +196,13 @@ class AirCargoProblem(Problem):
         pg_levelsum = pg.h_levelsum()
         return pg_levelsum
 
-    @lru_cache(maxsize=8192)
+    def h_pg_setlevel(self, node: Node):
+        # uses the planning graph setlevel heuristic calculated
+        # from this node to the goal
+        pg = PlanningGraph(self, node.state)
+        pg_setlevel = pg.h_setlevel()
+        return pg_setlevel
+        
     def h_ignore_preconditions(self, node: Node):
         """This heuristic estimates the minimum number of actions that must be
         carried out from the current state in order to satisfy all of the goal
